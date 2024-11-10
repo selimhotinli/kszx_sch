6541ed93dde136861d1361d412200b3d13d72910
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2024-11-10 17:23:37 -0500

    reorganize: move lss.py, cmb.py into core.py, import into top-level namespace

--- kszx/__init__.py
+++ kszx/__init__.py
@@ -1,5 +1,22 @@
 from . import cpp_kernels
 
+from .core import \
+    fft_r2c, \
+    fft_c2r, \
+    interpolate_points, \
+    grid_points, \
+    multiply_rfunc, \
+    multiply_kfunc, \
+    multiply_r_component, \
+    apply_partial_derivative, \
+    simulate_white_noise, \
+    simulate_gaussian_field, \
+    estimate_power_spectrum, \
+    kbin_average, \
+    fkp_from_ivar_2d, \
+    ivar_combine, \
+    estimate_cl
+
 from . import utils
 from . import io_utils
 from . import healpix_utils
@@ -15,7 +32,5 @@ from . import desils
 from . import desils_lrg
 from . import planck
 from . import sdss
-
-from . import cmb
-from . import lss
 from . import tests
+
