3c1f6210fcd53594dcd1484c48e94382aa527894
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2024-12-10 18:20:43 -0500

    minor changes to SurrogateFactory

--- kszx/SurrogateFactory.py
+++ kszx/SurrogateFactory.py
@@ -15,7 +15,7 @@ from .core import \
 
 
 class SurrogateFactory:
-    def __init__(self, box, cosmo, ngal, bg, randcat, rweights=1.0, fnl=0, fsurr=1, ksz=False, bv=None, kernel='cubic'):
+    def __init__(self, box, cosmo, ngal, bg, randcat, rweights=1.0, photometric=None, fnl=0, fsurr=1, ksz=False, bv=None, kernel='cubic'):
         """
         rweights: either None, or a 1-d array of length randcat.size (e.g. FKP weights).
 
@@ -42,33 +42,43 @@ class SurrogateFactory:
         self.ngal = ngal
         self.kernel = kernel
         self.nrand = randcat.size
-        self.nsurr = int(fsurr * self.nrand)
+        self.nsurr = int(fsurr * self.nrand + 0.5)
         self.fnl = fnl
         self.ksz = ksz
         # Note: don't need to save reference to 'randcat'.
 
+        # Initialize ztrue_col, zobs_col
         # Determine whether survey is spectroscopic or photometric (see docstring).
         zcols = tuple([ (x in randcat.col_names) for x in ['z','ztrue','zobs'] ])
+
         if zcols == (True,False,False):
-            photo = False
+            if photometric is None:
+                print("SurrogateFactory: setting photometric=False (catalog contains 'z' but not ztrue/zobs)")
+                photometric = False
+            elif photometric:
+                raise RuntimeError("photometric=True was specified, but randcat only contains 'z' column")
+            ztrue_col = 'z'
+            zobs_col = 'z'
         elif zcols == (False,True,True):
-            photo = True
+            if photometric is None:
+                print("SurrogateFactory: setting photometric=True (catalog contains ztrue/zobs but not 'z')")
+                photometric = True
+            elif not photometric:
+                print("SurrogateFactory: photometric=False was specified, setting zobs=ztrue")
+            ztrue_col = 'ztrue'
+            zobs_col = 'zobs' if photometric else 'ztrue'
         else:
             raise RuntimeError("Expected one of two cases: (1) randcat contains 'z' but not ('ztrue','zobs'),"
                                 + " or (2) randcat contains ('ztrue','zobs') but not 'z'. Specified randcat"
                                 + f" contains the following columns: {sorted(randcat.col_names)}")
 
-        self.photometric = photo
-        self.rcat_ztrue = randcat.ztrue if photo else randcat.z
-        self.rcat_zobs = randcat.zobs if photo else randcat.z
-        self.rcat_xyz_true = randcat.get_xyz(cosmo,zcol_name=('ztrue' if photo else 'z'))
-        self.rcat_xyz_obs = randcat.get_xyz(cosmo,zcol_name='zobs') if photo else self.rcat_xyz_true
-        self.rcat_wt = utils.asarray(rweights, 'SurrogateFactory', 'rweights', dtype=float)
-        assert (self.rcat_wt.ndim == 0) or (self.rcat_wt.shape == (self.nrand,))
-
+        rcat_ztrue = getattr(randcat, ztrue_col)
+        self.rcat_xyz_true = randcat.get_xyz(cosmo, zcol_name = ztrue_col)
+        self.rcat_xyz_obs = randcat.get_xyz(cosmo, zcol_name = zobs_col)
+        
         # Initialize rcat_bg (two cases, since 'bg' can be either a function or a scalar).
         if callable(bg):
-            rcat_bg = bg(self.rcat_ztrue)    # Note ztrue here
+            rcat_bg = bg(rcat_ztrue)    # Note ztrue here
             rcat_bg = np.asarray(rcat_bg, dtype=float)
             if rcat_bg.shape != (self.nrand,):
                 raise RuntimeError(f"Return value from bg() has shape {rcat_bg.shape}, expected shape {(self.nrand,)}")
@@ -77,8 +87,11 @@ class SurrogateFactory:
             if rcat_bg.ndim != 0:
                 raise RuntimeError(f"Expected 'bg' arg to be either a function or a scalar (got shape {rcat_bg.shape})")
 
+        self.photometric = photometric
         self.sigma2 = self._integrate_kgrid(self.box, cosmo.Plin_z0(self.box.get_k()))
-        self.rcat_bD = rcat_bg * cosmo.D(z=self.rcat_ztrue, z0norm=True)   # Note ztrue here
+        self.rcat_bD = rcat_bg * cosmo.D(z=rcat_ztrue, z0norm=True)   # Note ztrue here
+        self.rcat_wt = utils.asarray(rweights, 'SurrogateFactory', 'rweights', dtype=float)
+        assert (self.rcat_wt.ndim == 0) or (self.rcat_wt.shape == (self.nrand,))
 
         v = self.sigma2 * self.rcat_bD**2
         self.fsurr_min = (ngal/self.nrand * np.max(v)) + 1.0e-10
@@ -122,11 +135,11 @@ class SurrogateFactory:
             surr_wt = self.rcat_wt
             surr_eta_rms = self.rcat_eta_rms
         else:
-            isrc = np.random.permutation(self.nrand)[:nself.nsurr]
+            isrc = np.random.permutation(self.nrand)[:self.nsurr]
             surr_xyz_true = self.rcat_xyz_true[isrc]
             surr_xyz_obs = self.rcat_xyz_obs[isrc]
             surr_bD = self.rcat_bD[isrc]
-            surr_wt = self.rcat_wt[isrc]  
+            surr_wt = self.rcat_wt[isrc] if (self.rcat_wt.ndim > 0) else self.rcat_wt
             surr_eta_rms = self.rcat_eta_rms[isrc]
         
         # Step 2. Evaluate fields:
