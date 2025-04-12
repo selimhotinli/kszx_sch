4e67228282178785a4dcbd76b927000419f90be4
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2025-04-12 16:08:51 -0400

    kszx_desils.RegulatedDeconvolver

--- kszx/ksz_desils.py
+++ kszx/ksz_desils.py
@@ -189,7 +189,16 @@ class PhotozDistribution:
 
 class Kpipe:
     def __init__(self, input_dir, output_dir, nsurr=400):
-        """FIXME write comment explaining inputs/outputs."""
+        """Kendrick KSZ P(k) pipeline.
+
+        Runnable either by calling the run() method, or from the command line with:
+           python -m kszx <input_dir> <output_dir>
+
+        Reminder: input directory must contain files { params.yml, galaxies.h5, randoms.h5,
+        bounding_box.pkl }. The pipeline will populate the output directory with files
+        { params.yml, pk_data.npy, pk_surrogates.npy }. (For details of these file formats,
+        see one of the README files that Kendrick or Selim have lying around.)
+        """
         
         self.input_dir = input_dir
         self.output_dir = output_dir
@@ -208,30 +217,47 @@ class Kpipe:
             self.kbin_centers = (self.kbin_edges[1:] + self.kbin_edges[:-1]) / 2.
 
         self.box = io_utils.read_pickle(f'{input_dir}/bounding_box.pkl')
-        self.cosmo = Cosmology('planck18+bao')
-        self.gcat = Catalog.from_h5(f'{input_dir}/galaxies.h5')        
-        self.rcat = Catalog.from_h5(f'{input_dir}/randoms.h5')
 
-        # FIXME needs comment
-        self.surr_ngal_mean = self.gcat.size
-        self.surr_ngal_rms = 4 * np.sqrt(self.gcat.size)  # 4x Poisson
+        # This code moved into cached properties (see below), in order to reduce startup time.
+        # self.cosmo = Cosmology('planck18+bao')
+        # self.gcat = Catalog.from_h5(f'{input_dir}/galaxies.h5')        
+        # self.rcat = Catalog.from_h5(f'{input_dir}/randoms.h5')
 
         self.pk_data_filename = f'{output_dir}/pk_data.npy'
         self.pk_surr_filename = f'{output_dir}/pk_surrogates.npy'
         self.pk_single_surr_filenames = [ f'{output_dir}/tmp/pk_surr_{i}.npy' for i in range(nsurr) ]
 
-        
+
+    @functools.cached_property
+    def cosmo(self):
+        return Cosmology('planck18+bao')
+
+    @functools.cached_property
+    def gcat(self):
+        return Catalog.from_h5(f'{self.input_dir}/galaxies.h5')
+
+    @functools.cached_property
+    def rcat(self):
+        return Catalog.from_h5(f'{self.input_dir}/randoms.h5')
+
+    
     @functools.cached_property
     def pse(self):
+        """Returns a KszPSE object."""
+        
         print('Initializing KszPSE: this will take a few minutes')
+        
+        # FIXME needs comment
+        surr_ngal_mean = self.gcat.size
+        surr_ngal_rms = 4 * np.sqrt(self.gcat.size)  # 4x Poisson
 
         pse = KszPSE(
             box = self.box, 
             cosmo = self.cosmo, 
             randcat = self.rcat, 
             kbin_edges = self.kbin_edges,
-            surr_ngal_mean = self.surr_ngal_mean,
-            surr_ngal_rms = self.surr_ngal_rms,
+            surr_ngal_mean = surr_ngal_mean,
+            surr_ngal_rms = surr_ngal_rms,
             surr_bg = self.surr_bg,
             rweights = self.rcat.weight_zerr,
             nksz = 2,
@@ -247,7 +273,12 @@ class Kpipe:
 
     
     def get_pk_data(self, run=False):
-        print(f'get_pk_data(), time={time.time()}\n', end='')
+        """Returns a shape (3,3,nkbins) array.
+
+        If run=False, then this function expects the P(k) file to be on disk from a previous pipeline run.
+        If run=True, then the P(k) file will be computed if it is not on disk.
+        """
+        
         if os.path.exists(self.pk_data_filename):
             return io_utils.read_npy(self.pk_data_filename)
         
@@ -271,7 +302,12 @@ class Kpipe:
 
 
     def get_pk_surrogate(self, isurr, run=False):
-        print(f'get_pk_surrogate(), time={time.time()}\n', end='')
+        """Returns a shape (6,6,nkbins) array.
+        
+        If run=False, then this function expects the P(k) file to be on disk from a previous pipeline run.
+        If run=True, then the P(k) file will be computed if it is not on disk.
+        """
+        
         fname = self.pk_single_surr_filenames[isurr]
         
         if os.path.exists(fname):
@@ -280,7 +316,6 @@ class Kpipe:
         if not run:
             raise RuntimeError(f'Kpipe.get_pk_surrogate(): run=False was specified, and file {fname} not found')
 
-        print(f'Running {fname}, time={time.time()}\n', end='')
         self.pse.simulate_surrogate()
         
         for sv in [ self.pse.Sv_noise, self.pse.Sv_signal ]:
@@ -295,6 +330,8 @@ class Kpipe:
 
 
     def get_pk_surrogates(self):
+        """Returns a shape (nsurr,6,6,nkins) array, containing P(k) for all surrogates."""
+        
         if os.path.exists(self.pk_surr_filename):
             return io_utils.read_npy(self.pk_surr_filename)
 
@@ -308,6 +345,8 @@ class Kpipe:
 
 
     def run(self, processes=2):
+        """Runs pipeline. If any output files already exist, they will be skipped."""
+        
         # Copy yaml file from input to output dir.
         if not os.path.exists(f'{self.output_dir}/params.yml'):
             shutil.copyfile(f'{self.input_dir}/params.yml', f'{self.output_dir}/params.yml')
@@ -327,7 +366,9 @@ class Kpipe:
         # Initialize KszPSE before creating multiprocessing Pool.
         self.pse
 
-        # FIXME change number of workers
+        # FIXME currently I don't have a good way of setting the number of processes automatically --
+        # caller must adjust the number of processes to the amount of memory in the node.
+        
         with utils.Pool(processes) as pool:
             l = [ ]
                 
@@ -342,3 +383,158 @@ class Kpipe:
         if not have_surr:
             # Consolidates all surrogates into one file
             self.get_pk_surrogates()
+
+
+####################################################################################################
+
+
+class RegulatedDeconvolver:
+    def __init__(self, zobs_vec, zerr_vec, zbin_width, soft_zmax=None):
+        """A photo-z error deconvolver which avoids the oscillatory behavior of Lucy-Richardson.
+        
+        Given the observed 2-d (zobs,zerr) distribution, models the 3-d (ztrue,zobs,zerr) distribution.
+        FIXME: needs comments explaining how it works!
+        
+        - zobs_vec: array of shape (ngal,) containing observed (photometric) redshifts.
+
+        - zerr_vec: array of shape (ngal,) containing estimated photo-z errors.
+
+        - zbin_width (scalar): used internally for binning. (Recommend ~0.01 for DESILS-LRG.)
+
+        - soft_zmax (scalar): used internally; galaxies with (zobs > soft_zmax) are discarded.
+          Should be chosen significantly larger than the max redshift of interest.
+        
+          (For DESILS-LRG with zmax=1, recommend soft_zmax=1.5. If this parameter is omitted,
+          then you'll get a tail of rare galaxies with z~5, which is annoying.)
+        """
+        
+        assert zobs_vec.ndim == 1
+        assert zobs_vec.shape == zerr_vec.shape
+        assert np.all(zobs_vec >= 0)
+        assert np.all(zerr_vec > 0)
+
+        if soft_zmax is not None:
+            mask = (zobs_vec <= soft_zmax)
+            zobs_vec = zobs_vec[mask]
+            zerr_vec = zerr_vec[mask]
+        
+        self.zobs_vec = zobs_vec
+        self.zerr_vec = zerr_vec
+        self.zbin_width = zbin_width
+        self.zmin = np.min(zobs_vec)
+        self.zmax = np.max(zobs_vec)
+        self.soft_zmax = soft_zmax
+
+        self.nzbins = np.array(self.zmax/zbin_width, dtype=int)
+        self.zbin_edges = np.arange(self.nzbins+1) * zbin_width
+        
+        # The variable y = log(zerr + zbin_width) is used to define zerr bins.
+        y = np.log(zerr_vec + self.zbin_width)
+        self.ymin = np.min(y) - 1.0e-8
+        self.ymax = np.max(y) + 1.0e-8
+        self.nybins = int((self.ymax - self.ymin) / 0.2) + 1
+        self.ybin_width = (self.ymax - self.ymin) / self.nybins
+        
+        iz = np.array(zobs_vec / zbin_width, dtype=int)
+        iz = np.clip(iz, 0, self.nzbins-1)
+
+        iy = np.array((y - self.ymin) / self.ybin_width, dtype=int)
+        iy = np.clip(iy, 0, self.nybins-1)
+
+        # self.pobs[iy,iz] is an array of shape (self.nybins, self.nzbins).
+        self.pobs = np.bincount(iy*self.nzbins + iz, minlength=self.nybins*self.nzbins)
+        self.pobs = np.reshape(self.pobs, (self.nybins, self.nzbins))
+        self.pobs += 1   # regulate
+        self.pobs = self.pobs / np.sum(self.pobs,axis=1).reshape((-1,1))  # normalize to PDF in z-bins (for each y-bin)
+
+
+    def _sample(self, zobs, zerr):
+        """Helper for sample(): Sample from conditional distribution P(ztrue|zobs,zerr)."""
+        
+        n = len(zobs)
+        nb = self.nzbins
+        rng = np.random.default_rng()
+
+        # iy, iz = bin indices (shape (n,))
+        # Reminder: the variable y = log(zerr + zbin_width) is used to define zerr bins.
+        y = np.log(zerr + self.zbin_width)
+        iy = np.array((y - self.ymin) / self.ybin_width, dtype=int)
+        iy = np.clip(iy, 0, self.nybins-1)
+        
+        # pobs = shape(n,nb)
+        pobs = self.pobs[iy]
+        assert pobs.shape == (n,nb)
+
+        # cdf = shape (n,nb+1), not "reweighted by pobs".
+        cdf = self.C((self.zbin_edges.reshape((1,-1)) - zobs.reshape((-1,1))) / zerr.reshape((-1,1)))
+        assert cdf.shape == (n,nb+1)
+        
+        # pdf = shape (n,nb), "reweighted by pobs"
+        pdf = cdf[:,1:] - cdf[:,:-1]
+        pdf *= pobs
+        pdf /= np.sum(pdf,axis=1).reshape((-1,1))
+
+        # ztrue_bins has shape (n,). I don't think there's any way to vectorize this loop.
+        ztrue_bins = np.array([ rng.choice(nb, p=pdf[i,:]) for i in range(n) ])
+        
+        cdf_lo = cdf[np.arange(n), ztrue_bins]
+        cdf_hi = cdf[np.arange(n), ztrue_bins+1]
+        c = cdf_lo + (cdf_hi-cdf_lo) * rng.uniform(size=n)
+        ztrue = zobs + zerr * self.Cinv(c)
+
+        return ztrue
+
+        
+    def sample(self, n, zobs_min=None, zobs_max=None):
+        """Returns (ztrue, zobs, zerr), where all 3 arrays are shape (n,).'
+        If n is large, you may want to call sample_parallel() instead."""
+
+        zobs_min = zobs_min if (zobs_min is not None) else (self.zmin - 0.01)
+        zobs_max = zobs_max if (zobs_max is not None) else (self.zmax + 0.01)
+        mask = np.logical_and(self.zobs_vec >= zobs_min, self.zobs_vec <= zobs_max)
+        rng = np.random.default_rng()
+        
+        # Shape (n,)
+        ix = np.nonzero(mask)[0]
+        ix = rng.choice(ix, size=n, replace=True)
+        zobs = self.zobs_vec[ix]
+        zerr = self.zerr_vec[ix]
+        ztrue = np.zeros(n)
+        pos = 0
+
+        # Compute in batches, to keep memory usage under control.
+        while pos < n:
+            pos2 = min(n, pos + 10**5)
+            ztrue[pos:pos2] = self._sample(zobs[pos:pos2], zerr[pos:pos2])
+            pos = pos2
+                
+        return ztrue, zobs, zerr
+
+
+    def sample_parallel(self, n, zobs_min=None, zobs_max=None, processes=None):
+        """Returns (ztrue, zobs, zerr), where all 3 arrays are shape (n,).
+        
+        Uses a multiprocessing Pool for speed. If 'processes' is None, then a
+        sensible default will be chosen."""
+
+        with utils.Pool(processes) as pool:
+            p = pool._processes
+            nlist = [ (((i+1)*n)//p) - ((i*n)//p) for i in range(p) ]
+            m = pool.starmap_async(self.sample, [(n,zobs_min,zobs_max) for n in nlist])
+            result = m.get()  # list of (ztrue, zobs, zerr) triples
+            
+        # Concatenate results from each task.
+        ztrue = np.concatenate([r[0] for r in result])
+        zobs = np.concatenate([r[1] for r in result])
+        zerr = np.concatenate([r[2] for r in result])
+
+        return ztrue, zobs, zerr
+
+
+    @classmethod
+    def C(cls, x):
+        return (1/2) * scipy.special.erf(x / np.sqrt(2.))
+
+    @classmethod
+    def Cinv(cls, y):
+        return scipy.special.erfinv(2*y) * np.sqrt(2.)
